3e1e67a01dadaa3003b3ff17bc196d2668125c1c
Jessica Oboh <jessicarichardson@Jessicas-MBP.lan>
2022-10-27 13:29:25 -0700

    functions to display weather

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,8 @@ function getWeather(event) {
         .then(function (response) {
             return response.json();
         })
+        .catch(function(response){
+            alert("Not a valid City");}
         .then(function (data) {
             console.log(data)
 
@@ -25,10 +27,25 @@ function getWeather(event) {
             dateEl.text(dateFormat);
             currentEl.append(dateEl);
 
-            var icon = data.weather.icon;
+            var icon = data.weather[0].icon;
             var iconEl = $('<img>');
-            //iconEl.attr(icon);
+            iconEl.attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
             currentEl.append(iconEl);
+
+            var temp = data.main.temp;
+            var tempEL = $("<p>");
+            tempEL.text("Temperature: " + temp + " °F")
+            currentEl.append(tempEL);
+
+            var humidity = data.main.humidity;
+            var humidityEL = $("<p>");
+            humidityEL.text("Humidity: " + humidity + " %")
+            currentEl.append(humidityEL);
+
+            var windSpeed = data.wind.speed;
+            var windEL = $("<p>");
+            windEL.text("Wind Speed: " + windSpeed + "  MPH")
+            currentEl.append(windEL);
         });
     getForecast();
 };
@@ -40,20 +57,42 @@ function getForecast() {
         })
         .then(function (data) {
             console.log(data)
-            var forecastDate = data.list[0].dt;
-            var dateFormat = new Date(forecastDate * 1000).toLocaleDateString();
-            var forecastDateEl = $('<p>');
-            forecastDateEl.text(dateFormat);
-            futureEl.append(forecastDateEl);
+            var header = $("<h3>");
+                header.text("5 Day Forecast");
+                futureEl.append(header);
 
-            var forecastTemp = data.list[0].main.temp
-            var forecastTempEl = $('<p>');
-            forecastTempEl.text(forecastTemp + "°F");
-            futureEl.append(forecastTempEl);
-            // for (var i = 0; i < data.list.length; i + 8) {
-               
+            for (var i = 0; i < data.list.length; i = i + 8) {
 
-            // }
+                var forecastCard = $("<div>");
+                forecastCard.attr("class", "card col-2 m-1 bg-secondary text-white");
+                futureEl.append(forecastCard);
+
+                var forecastDate = data.list[i].dt;
+                var dateFormat = new Date(forecastDate * 1000).toLocaleDateString();
+                var forecastDateEl = $('<p>');
+                forecastDateEl.text(dateFormat);
+                forecastCard.append(forecastDateEl);
+
+                var forecastIcon = data.list[i].weather[0].icon;
+                var forecastIconEl = $('<img>');
+                forecastIconEl.attr("src", "http://openweathermap.org/img/wn/" + forecastIcon + "@2x.png");
+                forecastCard.append(forecastIconEl);
+
+                var forecastTemp = data.list[i].main.temp;
+                var forecastTempEl = $('<p>');
+                forecastTempEl.text("Temperature: " + forecastTemp + " °F");
+                forecastCard.append(forecastTempEl);
+
+                var forecastHumidity = data.list[i].main.humidity;
+                var forecastHumidityEl = $('<p>');
+                forecastHumidityEl.text("Humidity: " + forecastHumidity + " %");
+                forecastCard.append(forecastHumidityEl);
+
+                var forecastWind = data.list[i].wind.speed
+                var forecastWindEl = $('<p>');
+                forecastWindEl.text("Wind Speed: " + forecastWind + "  MPH");
+                forecastCard.append(forecastWindEl)
+            }
         });
 
 };
